5a5cc54392fa615fcf171f250698bac1646ec1c3
Amit Kumar <33446537+amit-mnnit@users.noreply.github.com>
2018-03-25 17:51:41 +0530

    Update sidebar.controller.js

--- app/sidebar/sidebar.controller.js
+++ app/sidebar/sidebar.controller.js
@@ -2,6 +2,61 @@
     var App = angular.module('sideNavApp');
     App.controller('SideNavController', function ($rootScope, $scope) {
         var sideMenuListArray = [
+                    {
+                "Software Products": [
+                    {
+                        "Programming Languages": [
+                            "Java API",
+                            "Python API"
+                        ]
+                    },
+                    {
+                        "Documentation": [
+                            "Java Docs",
+                            "Python Docs",
+                            {
+                                "HTTP Docs": [
+                                    "REST Docs",
+                                    "SOAP Docs"
+                                ]
+                            }
+                        ]
+                    },
+                    "Try It!"
+                ]
+            },
+            {
+                "Cloud Products": [
+                    "SAAS",
+                    "PAAS",
+                    "IAAS"
+                ]
+            },
+            {
+                "About Us": [
+                    {
+                        "Locations": [
+                            {
+                                "India": [
+                                    "Delhi",
+                                    {
+                                        "Mumbai": [
+                                            "Lower Parel",
+                                            "Goregaon"
+                                        ]
+                                    },
+                                    "Bangalore"
+                                ]
+                            },
+                            "USA",
+                            "Singapore"
+                        ]
+                    },
+                    "Founders",
+                    "Advisors"
+                ]
+            },
+            "Contact Us"
         ];
         var tmp = [];
         function setData(data, tmp) {
